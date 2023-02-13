e38b2bd87df0ba20d1a8cd9a53a6b69e1d2576c9
George Muller <gotchie333@hotmail.com>
2023-02-12 22:15:06 -0500

    url render

--- client/src/components/chat/Form.jsx
+++ client/src/components/chat/Form.jsx
@@ -14,7 +14,7 @@ const [loading, setLoading] = useState(false);
 
 const messageResponse = async () => {
 setLoading(true);
-const { data } = await axios.post('https://max-971a.onrender.com/message', {
+const { data } = await axios.post('https://max-wlrm.onrender.com/message', {
 message
 });
 setLoading(false);
